534d5ba1114c2b3fc5ad7186e7fc251bde8910ae
Hieu Tu <hieu.tuminh@evolutionwellnesssolutionsg.onmicrosoft.com>
2022-11-29 13:37:00 +0700

    Update loader url

--- utils/imgLoader.ts
+++ utils/imgLoader.ts
@@ -1,7 +1,7 @@
 import { ImageLoaderProps } from "next/image";
 
-const imageLoader = ({src}: ImageLoaderProps) => {
-    return `/${src}`; 
-}
+const imageLoader = ({ src }: ImageLoaderProps) => {
+  return `https://raw.githubusercontent.com/kanetu/portfolio/main/public/${src}`;
+};
 
-export default imageLoader
+export default imageLoader;
