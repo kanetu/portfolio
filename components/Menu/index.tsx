8c6e38c0a0000799587641199e7719698d58be02
= <=>
2023-10-31 17:47:01 +0700

    rename folder

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { BlogIcon, CloseIcon, MailIcon, UserIcon, WorkIcon } from "../Icons";
+import { BlogIcon, CloseIcon, UserIcon, WorkIcon } from "../Icons";
 
 interface IMenu {
   isShowMenu: boolean;
@@ -27,31 +27,26 @@ const Menu: React.FC<IMenu> = ({ isShowMenu, onClickMenu }) => {
       >
         <CloseIcon />
       </button>
-      <div className="wrapper grid grid-rows-2 grid-flow-col w-[15.625em] gap-4">
+      <div className="wrapper mt-[2em] grid grid-cols-2 w-[20em] md:w-[25em] gap-[2em] md:gap-[4em]">
         <Link href="/about" onClick={onClickMenu}>
-          <div className="w-[7.5em] h-[7.5em] shadow-project flex flex-col justify-center items-center rounded-[0.5em]">
+          <div className="w-[8.5em] h-[8.5em] md:w-[10.5em] md:h-[10.5em] shadow-project flex flex-col justify-center items-center rounded-[0.5em]">
             <UserIcon className="w-[4.375em] h-[4.375em]" />
             <span>About</span>
           </div>
         </Link>
         <Link href="/blog" onClick={onClickMenu}>
-          <div className="w-[7.5em] h-[7.5em] shadow-project flex flex-col justify-center items-center rounded-[0.5em]">
+          <div className="w-[8.5em] h-[8.5em] md:w-[10.5em] md:h-[10.5em] shadow-project flex flex-col justify-center items-center rounded-[0.5em]">
             <BlogIcon className="w-[4.375em] h-[4.375em]" />
             <span>Blog</span>
           </div>
         </Link>
-        <Link href="/works" onClick={onClickMenu}>
-          <div className="w-[7.5em] h-[7.5em] shadow-project flex flex-col justify-center items-center rounded-[0.5em]">
+        <Link href="/works-and-contact" onClick={onClickMenu}>
+          <div className="w-[8.5em] h-[9.5em] md:w-[10.5em] md:h-[10.5em] shadow-project flex flex-col justify-center items-center rounded-[0.5em]">
             <WorkIcon className="w-[4.375em] h-[4.375em]" />
-            <span>Works</span>
+            <span>Works  & Contact</span>
           </div>
         </Link>
-        <Link href="/contact" onClick={onClickMenu}>
-          <div className="w-[7.5em] h-[7.5em] shadow-project flex flex-col justify-center items-center rounded-[0.5em]">
-            <MailIcon className="w-[4.375em] h-[4.375em]" />
-            <span>Contact</span>
-          </div>
-        </Link>
+        
       </div>
     </div>
   );
